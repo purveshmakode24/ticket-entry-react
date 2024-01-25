d4ecb2e750c03471ed7ce0d60c372a5c4f88d879
PURVESH <makodepurvesh@gmail.com>
2024-01-26 03:38:35 +0530

    Update config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,10 +1,5 @@
-declare const window: any;
-
 export const DEBUG = false; // Keep false in production
-export const REACT_APP_VERSION = window.REACT_APP_VERSION as string;
-console.log('DEBUG', DEBUG);
+export const APP_VERSION = '1.0.0';
 export const API_ROOT_URL = DEBUG ?
     'http://localhost:8000/api/' :
-    'https://ticket-entry-backend.vercel.app/api/' as string;
-
-console.log('API_ROOT_URL', API_ROOT_URL);
+    'https://ticket-entry-backend.vercel.app/api/';
